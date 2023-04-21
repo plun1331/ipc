46b38f121d0d8864b3b1f860b0becad31838bf0a
plun1331 <49261529+plun1331@users.noreply.github.com>
2023-04-21 17:42:59 +0000

    add licenses

--- ipc/__init__.py
+++ ipc/__init__.py
@@ -1,2 +1,12 @@
+"""
+ipc
+~~~~~~~~~~~~~
+
+A simple, secure, and fast inter-process communication library for Python.
+
+:copyright: (c) 2023-present plun1331
+:license: BSD 3-Clause, see LICENSE for more details.
+"""
+
 from .client import Client
 from .server import Server
